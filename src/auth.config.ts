4719b3c3052f8658c8fcd3fadf58214e5e025359
baeckkevin07-blip <baeck.kevin07@gmail.com>
2025-12-02 15:22:38 +0100

    Update src/auth.config.ts for debug

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,5 +2,18 @@ export const authConfig = {
     pages: {
         signIn: '/login',
     },
+    callbacks: {
+        authorized({ auth, request: { nextUrl } }) {
+            const isLoggedIn = !!auth?.user
+            const isOnAdmin = nextUrl.pathname.startsWith('/admin')
+
+            if (isOnAdmin) {
+                if (isLoggedIn) return true
+                return false // Redirect unauthenticated users to login page
+            }
+
+            return true
+        },
+    },
     providers: [],
 }
